9aeb6b0c962ac1afdca5e4ffaab7c2e6f3523b51
Mazstar04 <mazeedahhamzat@gmail.com>
2022-01-14 12:13:07 +0100

    init

---
+++ DistanceAmongAllPairs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestPossibleSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] arrayOfInt = new int[]{1,6,1};
+            distanceAmongAllPairs(arrayOfInt, 3);
+            
+
+        }
+
+        public static void distanceAmongAllPairs (int[] arrayOfInt, int k)
+        {
+            List<int> diff = new List<int>();
+
+            for(int i = 0; i<arrayOfInt.Length; i++)
+            {
+                for(int j = i+1; j< arrayOfInt.Length; j++)
+                {
+                    if(j != i && i != arrayOfInt.Count()-1)
+                    {
+                     diff.Add(Math.Abs(arrayOfInt[i] - arrayOfInt[j]));
+                    }
+                }
+            }
+            diff.Sort();
+            Console.WriteLine(diff[k-1]);
+        }
+    }
+}
---
+++ LongestPossibleSubstring/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LongestPossibleSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "croboorocoboo";
+            LongestSubstring(word);
+        }
+
+        public static void LongestSubstring(string word)
+        {
+            string replica = word;
+            string t = "";
+            string subString = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                t = replica[0].ToString();
+                replica = replica.Remove(0, 1);
+
+
+                if (subString == "")
+                {
+                    if (replica.Contains(t))
+                    {
+                        subString += t;
+                    }
+                    continue;
+                }
+                else
+                {
+                    if (replica.Contains(subString + t))
+                    {
+                        subString += t;
+
+                    }
+                }
+            }
+            Console.WriteLine(subString);
+        }
+    }
+}
+
+
